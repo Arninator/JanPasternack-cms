97a9eb981ce80c1be311803af16dffadb6f13fe6
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-12-15 20:40:26 +0100

    h1 under te img and add margin bottom

--- src/pages/Aktuelles/index.js
+++ src/pages/Aktuelles/index.js
@@ -15,7 +15,7 @@ export default class AktuellesBlogIndexPage extends React.Component {
             backgroundPosition: "0% 33%"
           }}
         >
-          <h1
+          {/* <h1
             className="has-text-weight-bold is-size-1"
             style={{
             //   boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
@@ -26,10 +26,22 @@ export default class AktuellesBlogIndexPage extends React.Component {
             }}
           >
             Aktuelles
-          </h1>
+          </h1> */}
         </div>
         <section className="section">
           <div className="container">
+            <h1
+              className="has-text-weight-bold is-size-1 flex center"
+              style={{
+                // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+                // backgroundColor: "transparent",
+                color: "black",
+                padding: "1rem",
+                marginBottom: "8vh"
+              }}
+            >
+              Aktuelles
+            </h1>
             <div className="content">
               <AktuellesBlogRoll />
             </div>
