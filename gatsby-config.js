61fd9f4f1271b41b558c2e540fdd853e556cb3f6
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-12-26 13:52:54 +0100

    change site metadata: Title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Jan Pasternack",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "Personal Website of Jan Pasternack, SPD"
+      // "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
