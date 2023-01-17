cafdf379d9c08f6873224b039759f17a99f4cbcd
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-01-17 17:31:43 +0100

    adjust shape of termine boxes

--- src/components/TermineBlogRoll.js
+++ src/components/TermineBlogRoll.js
@@ -8,57 +8,72 @@ class TermineBlogRollTemplate extends React.Component {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
-    // console.log(posts);
+    console.log(posts);
 
     return (
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
             <div className="is-parent column is-4" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
+              <Link
+                className=""
+                to={post.fields.slug}
               >
-                <header>
-                  {post.frontmatter.featuredimage ? (
-                    <div className="featured-thumbnail">
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                          width:
-                            post.frontmatter.featuredimage.childImageSharp
-                              .gatsbyImageData.width,
-                          height:
-                            post.frontmatter.featuredimage.childImageSharp
-                              .gatsbyImageData.height,
+                <article
+                  className={`blog-list-item tile is-child box notification hover ${
+                    post.frontmatter.featuredpost ? 'is-featured' : ''
+                  }`}
+                  style={{
+                    backgroundColor: "hsl(0, 0%, 93%)"
+                  }}
+                >
+                  <header className="flex column center">
+                    {post.frontmatter.featuredimage ? (
+                      <div 
+                        className="flex row center"
+                        style={{
+                          marginBottom: "1vh",
                         }}
-                      />
-                    </div>
-                  ) : null}
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.fields.slug}
+                      >
+                        <PreviewCompatibleImage
+                          imageInfo={{
+                            image: post.frontmatter.featuredimage,
+                            alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                          }}
+                        />
+                      </div>
+                    ) : null}
+                    <p 
+                      className="post-meta blog-title"
+                      style={{
+                        width: "100%"
+                      }}
                     >
                       {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
+                          <span></span>
+                          <span className="subtitle is-size-5 is-block">
+                            {post.frontmatter.date}
+                          </span>
+                      </p>
+                  </header>
+                  <p className="flex column center">
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <div
+                      className="flex row center"
+                      style={{
+                        width: "100%"
+                      }}
+                    >
+                      <Link className="button" to={post.fields.slug}>
+                        Show &rarr;
+                      </Link>
+                    </div>
+                    
                   </p>
-                </header>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button" to={post.fields.slug}>
-                    Weiterlesen Aktuell &rarr;
-                  </Link>
-                </p>
-              </article>
+                </article>
+              </Link>
             </div>
           ))}
       </div>
@@ -83,7 +98,7 @@ export default function TermineBlogRoll() {
       query={graphql`
         query TermineBlogRollQuery {
           allMarkdownRemark(
-            sort: { order: DESC, fields: [frontmatter___date] }
+            sort: { order: ASC, fields: [frontmatter___date] }
             filter: { frontmatter: { templateKey: { eq: "termine-page" } } }
           ) {
             edges {
@@ -101,7 +116,6 @@ export default function TermineBlogRoll() {
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(
-                        width: 120
                         quality: 100
                         layout: CONSTRAINED
                       )
