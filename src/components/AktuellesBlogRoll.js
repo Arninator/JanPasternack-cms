14b930a7389f3355618fbcad1520e156f75fd690
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-01-18 21:30:38 +0100

    just delete one empty linw

--- src/components/AktuellesBlogRoll.js
+++ src/components/AktuellesBlogRoll.js
@@ -109,7 +109,6 @@ export default function AktuellesBlogRoll() {
                         quality: 100
                         layout: CONSTRAINED
                       )
-
                     }
                   }
                 }
