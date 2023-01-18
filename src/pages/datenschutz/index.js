7680c1a2066d8df9c6236b346f000d40cbe8e58c
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-01-18 21:10:19 +0100

    create files

---
+++ src/pages/datenschutz/index.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+import Layout from "../../components/Layout";
+import DatenschutzBlogRoll from "../../components/DatenschutzBlogRoll";
+
+export default class DatenschutzBlogIndexPage extends React.Component {
+  render() {
+;
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: ``,
+            backgroundPosition: "0% 33%"
+          }}
+        >
+        </div>
+        <section className="section">
+          <div className="container">
+            <h1
+              className="has-text-weight-bold is-size-1 flex center"
+              style={{
+                // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+                // backgroundColor: "transparent",
+                color: "black",
+                padding: "1rem",
+                marginBottom: "8vh"
+              }}
+            >
+              Datenschutz
+            </h1>
+            <div className="content">
+              <DatenschutzBlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
---
+++ src/pages/impressum/index.js
@@ -0,0 +1,41 @@
+import * as React from "react";
+
+import Layout from "../../components/Layout";
+import AktuellesBlogRoll from "../../components/AktuellesBlogRoll";
+
+export default class AktuellesBlogIndexPage extends React.Component {
+  render() {
+;
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/aktuelles-cover.jpg')`,
+            backgroundPosition: "0% 33%"
+          }}
+        >
+        </div>
+        <section className="section">
+          <div className="container">
+            <h1
+              className="has-text-weight-bold is-size-1 flex center"
+              style={{
+                // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+                // backgroundColor: "transparent",
+                color: "black",
+                padding: "1rem",
+                marginBottom: "8vh"
+              }}
+            >
+              Aktuelles
+            </h1>
+            <div className="content">
+              <AktuellesBlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
