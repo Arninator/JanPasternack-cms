fd5a28dbd6e601ecfa92f47b2d3d727a14d62a28
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-01-31 17:35:21 +0100

    remove empty div over content

--- src/pages/impressum/index.js
+++ src/pages/impressum/index.js
@@ -1,37 +1,25 @@
 import * as React from "react";
 
 import Layout from "../../components/Layout";
-// import ImpressumBlogRoll from "../../components/ImpressumBlogRoll";
+import ImpressumBlogRoll from "../../components/ImpressumBlogRoll";
 
 export default class ImpressumBlogIndexPage extends React.Component {
   render() {
 ;
     return (
       <Layout>
-        <div
+        {/* <div
           className="full-width-image-container margin-top-0"
           style={{
             // backgroundImage: `url('/img/aktuelles-cover.jpg')`,
             backgroundPosition: "0% 33%"
           }}
         >
-        </div>
+        </div> */}
         <section className="section">
           <div className="container">
-            <h1
-              className="has-text-weight-bold is-size-1 flex center"
-              style={{
-                // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-                // backgroundColor: "transparent",
-                color: "black",
-                padding: "1rem",
-                marginBottom: "8vh"
-              }}
-            >
-              Impressum!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            </h1>
             <div className="content">
-              {/* <ImpressumBlogRoll /> */}
+              <ImpressumBlogRoll />
             </div>
           </div>
         </section>
