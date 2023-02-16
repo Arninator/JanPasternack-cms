7e47af5787cb79a257bd8c9ee0231014f81a9839
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-02-16 17:33:36 +0100

    layout the common contact page

--- src/pages/kontakt/index.js
+++ src/pages/kontakt/index.js
@@ -1,8 +1,7 @@
 import * as React from "react";
 
 import Layout from "../../components/Layout";
-import WahlkreisBlogRoll from "../../components/KontaktBlogRoll";
-import PressebilderBlogRoll from "../../components/PressebilderBlogRoll";
+import KontaktBlogRoll from "../../components/KontaktBlogRoll";
 
 export default class KontaktIndexPage extends React.Component {
   componentDidMount() {
@@ -20,30 +19,24 @@ export default class KontaktIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/20220504pj209.jpg')`,
+            backgroundPosition: "0% 100%",
           }}
         >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-              backgroundColor: "#f40",
-              color: "white",
-              padding: "1rem",
-            }}
-          >
-            Kontakt
-          </h1>
+          
         </div>
         <section className="section">
           <div className="container">
             <div className="content">
-              <WahlkreisBlogRoll />
-            </div>
-          </div>
-          <div className="container">
-            <div className="content">
-              <PressebilderBlogRoll />
+              <h1
+                className="has-text-weight-bold is-size-1 column is-9"
+                style={{
+                  color: "rgb(227, 0, 15)",
+                }}
+              >
+                Kontakt
+              </h1>
+              <KontaktBlogRoll />
             </div>
           </div>
         </section>
