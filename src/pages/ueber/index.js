20ec1fc89f6de0bdc2aecdb143a42f4dde8ae135
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-02-27 21:49:45 +0100

    do the layout of this common page

--- src/pages/ueber/index.js
+++ src/pages/ueber/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 import Layout from "../../components/Layout";
 import VorstellungBlogRoll from "../../components/VorstellungBlogRoll";
@@ -23,32 +24,55 @@ export default class UeberIndexPage extends React.Component {
           className="full-width-image-container margin-top-0"
           style={{
             backgroundImage: `url('/img/IMG-20220505-WA0001.jpg')`,
-            backgroundPosition: "0 25%"
+            backgroundPosition: "0 25%",
           }}
         >
-          <h1
-            className="has-text-weight-bold is-size-1 border"
-            style={{
-              // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-              // backgroundColor: "#f40",
-              color: "white",
-              padding: "1rem",
-              justifySelf: "baseline",
-              alignSelf: "end",
-            }}
-          >
-            Über mich
-          </h1>
         </div>
-        <section className="section">
+        <section className="section whiteBackground fade-in">
           <div className="container">
             <div className="content">
-              <VorstellungBlogRoll />
+              <h1
+                className="has-text-weight-bold is-size-1"
+                style={{
+                  // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+                  // backgroundColor: "#f40",
+                  // color: "white",
+                  padding: "1rem",
+                  // margin: "auto 2% auto 20%"
+                }}
+              >
+                Über mich
+              </h1>
             </div>
           </div>
-          <div className="container border">
+        </section>
+        <section className="section greyBackground fade-in">
+          <div className="container">
             <div className="content">
+              <h2 className="has-text-weight-semibold is-size-2">
+                Warum ich für den Hessischen Landtag kandidiere
+              </h2>
+              <VorstellungBlogRoll />
+            </div>
+            <div className="column is-12 has-text-centered">
+              <Link className="plain-button" to="/vorstellung">
+                Mehr... &rarr;
+              </Link>
+            </div>
+          </div>
+        </section>
+        <section className="section whiteBackground fade-in">
+          <div className="container">
+            <div className="content">
+              <h2 className="has-text-weight-semibold is-size-2">
+                Lebenslauf
+              </h2>
               <LebenslaufBlogRollExcerpt/>
+            </div>
+            <div className="column is-12 has-text-centered">
+              <Link className="plain-button" to="/lebenslauf">
+                Weiterlesen&nbsp;&rarr;
+              </Link>
             </div>
           </div>
         </section>
