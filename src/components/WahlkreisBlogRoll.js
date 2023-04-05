5df2ff136cb7ce9bbbdd4588fd938e5fb2e85d5c
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-04-05 13:45:10 +0200

    tryto adjust fontweight

--- src/components/WahlkreisBlogRoll.js
+++ src/components/WahlkreisBlogRoll.js
@@ -19,7 +19,7 @@ class WahlkreisBlogRollTemplate extends React.Component {
 
                 <h1>{ post.frontmatter.title }</h1>
 
-                <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+                <div dangerouslySetInnerHTML={{__html: post.html}} style={{fontWeight: "100"}}></div>
                 
               </article>
             </div>
