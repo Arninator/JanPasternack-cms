2bc4c1e23e69a9f282468d62b03d0fd33e3e8acf
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-04-15 15:22:05 +0200

    delete unused code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -220,25 +220,6 @@ const Navbar = class extends React.Component {
     this.setState({
       active: !this.state.active,
     })
-
-    // OLD
-    // // toggle the active boolean in the state
-    // this.setState(
-    //   {
-    //     active: !this.state.active,
-    //   },
-    //   // after state has been updated,
-    //   () => {
-    //     // set the class in state for the navbar accordingly
-    //     this.state.active
-    //       ? this.setState({
-    //           navBarActiveClass: "is-active",
-    //         })
-    //       : this.setState({
-    //           navBarActiveClass: "",
-    //         });
-    //   }
-    // );
   }
 
   render() {
