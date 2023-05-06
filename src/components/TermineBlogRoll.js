73c9da3dd05f54a8e6690109900bffb3d74f774a
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-05-06 12:19:53 +0200

    change termine sheets to presse styled sheets

--- src/components/TermineBlogRoll.js
+++ src/components/TermineBlogRoll.js
@@ -2,156 +2,101 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
-
-import "../components/style.css";
+import { GatsbyImage } from "gatsby-plugin-image";
+import $ from "jquery";
 
 class TermineBlogRollTemplate extends React.Component {
-  
+
   constructor(props) {
     super(props);
     this.state = {
-      buttonOption: false,
-      index: 1,
-      selection: 3,
+      columns: 12,
     };
   }
 
   componentWillMount() {
-    if (typeof window !== "undefined" && window.location.href.includes("termine")) {
-      this.setState({
-        buttonOption: true,
-      });
-    }
 
-    if (typeof window !== "undefined") {
+    if (typeof window !== "undefined" && (window.location.href.includes("termine") || window.location.href.includes("news"))) {
       if ( window.innerWidth > 992) {
         this.setState({
-          selection: 3,
+          columns: 4,
         });
       } else if ( window.innerWidth > 700 ) {
         this.setState({
-          selection: 2,
+          columns: 8,
         });
       } else {
         this.setState({
-          selection: 1,
+          columns: 12,
         });
       }
     }
   }
 
-  prevClick() {
-    this.setState({
-      index: this.state.index > 1 ? this.state.index - 1 : 1,
-    })
-  }
-
-  nextClick() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark;
-    this.setState({
-      index: (this.state.index < (posts.length - 2)) ? this.state.index + 1 : (posts.length - 2),
-    })
-  }
- 
   render() {
-    const { data } = this.props;
-    const { edges: posts } = data.allMarkdownRemark;
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="flex-row is-multiline">
-        {this.state.buttonOption && 
-          <div className='flex-column flex-center' style={{ height: "inherit" }}>
-            <button 
-              className='flex center invisible-button'
-              onClick={() => this.prevClick()}
-              style={{height: "10%", width: "2vw"}}
+      <div className="columns is-multiline">
+        {posts.length > 0 ?
+          posts.map(({ node: post }) => (
+            <div 
+              className={`is-parent column is-${ this.state.columns }`} 
+              key={post.id}
             >
-              {/* &#9665; */}
-              &larr;
-            </button>
-          </div>
-        }
-        {posts &&
-          posts.map(({ node: post }, index) => ((index <= (this.state.index + 1)) && (index >= (this.state.index + 1 - (this.state.selection - 1)))) ? (
-            <div className="is-parent column is-4" key={post.id} >
-              {/* {console.log("selection: " + this.state.selection)} */}
-              <Link
-                className=""
-                to={post.fields.slug}
+              <article
+                className={`blog-list-item tile is-child notification kachel ${
+                  post.frontmatter.featuredpost ? 'is-featured' : ''
+                }`}
               >
-                <article
-                  className={`blog-list-item tile is-child notification kachel ${
-                    post.frontmatter.featuredpost ? 'is-featured' : ''
-                  }`}
-                  style={{
-                    // aspectRatio: "1 / 1"
-                  }}
-                >
-                  <header className="flex-column flex-center">
-                    {post.frontmatter.featuredimage ? (
-                      <div 
-                        className="flex-row flex-center"
-                        style={{
-                          marginBottom: "1vh",
-                        }}
-                      >
-                        <PreviewCompatibleImage
-                          imageInfo={{
-                            image: post.frontmatter.featuredimage,
-                            alt: `featured image thumbnail for event ${post.frontmatter.title}`,
-                          }}
-                        />
-                      </div>
-                    ) : null}
-                    <p 
-                      className="post-meta blog-title"
-                      style={{
-                        width: "100%"
-                      }}
-                    >
-                      {post.frontmatter.title}
-                          <span></span>
-                          <span className="subtitle is-size-5 is-block">
-                            {post.frontmatter.date}
-                          </span>
-                      </p>
-                  </header>
-                  <p className="flex-column center">
-                    { post.excerpt }
-                    <br />
-                    <br />
-                    {/* <span href={post.frontmatter.location.link}>
-                      {post.frontmatter.location.name}
-                    </span> */}
-                    <br />
-                  </p>
-                  <span
-                    className="flex-row flex-center"
-                    style={{
-                      width: "100%"
+                <div style={{ fontWeight: "400" }}>{ post.frontmatter.title }<br /><br /><br /></div>
+                <header className="flex-column flex-center">
+                  <p 
+                    className="post-meta blog-title"
+                    style={{ 
+                      fontWeight: "600",
+                      fontSize: "24px"
                     }}
                   >
-                    <Link className="button" to={post.fields.slug}>
-                      Zeigen &rarr;
-                    </Link>
-                  </span>
-                </article>
-              </Link>
+                    { post.frontmatter.title }
+                  </p>
+                  { post.frontmatter.featuredimage ? (
+                    <div style={{ margin: "0 0 2vh 0" }}>
+                      <PreviewCompatibleImage
+                        imageInfo={{
+                          image: post.frontmatter.featuredimage,
+                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                        }}
+                      />
+                    </div>
+                  ) : null}
+                  <p style={{ margin: "0vh", fontWeight: "400" }}>
+                    { post.frontmatter.title }
+                  </p>
+                </header>
+                <p style={{ fontWeight: "100"}} >
+                  { post.excerpt }
+                  <br />
+                  <br />
+                </p>
+                <div className="flex-row flex-center">
+                  <Link className="button" to={ post.fields.slug }>
+                    Weiterlesen &rarr;
+                  </Link>
+                </div>
+              </article>
             </div>
-          ) : "" )}
-        {this.state.buttonOption && 
-          <div className='flex-column flex-center' style={{height: "inherit"}}>
-            <button 
-              className='flex center invisible-button'
-              onClick={() => this.nextClick()}
-              style={{height: "10%", width: "2vw"}}
-            >
-              {/* &#9655; */}
-              &rarr;
-            </button>
-          </div>
-        }
+          )) : 
+          <div 
+            className="is-parent column is-12"
+            style={{
+              margin: "1vw",
+              color: "darkolivegreen"
+            }}
+          >
+            Aktuell stehen keine Termine an...
+          </div>}
       </div>
     )
   }
@@ -174,41 +119,34 @@ export default function TermineBlogRoll() {
       query={graphql`
         query TermineBlogRollQuery {
           allMarkdownRemark(
-            sort: { order: ASC, fields: [frontmatter___date] }
+            sort: { order: DESC, fields: [frontmatter___date] }
             filter: { frontmatter: { templateKey: { eq: "termine-page" } } }
           ) {
             edges {
               node {
-                excerpt(pruneLength: 400)
+                excerpt(pruneLength: 100)
                 id
                 fields {
                   slug
                 }
                 frontmatter {
                   title
-                  templateKey
-                  eventlink
-                  date(formatString: "MMMM DD, YYYY")
-                  location {
-                    name
-                    link
-                  }
-                  featuredpost
+                  date
                   featuredimage {
                     childImageSharp {
                       gatsbyImageData(
-                        quality: 100
                         layout: CONSTRAINED
                       )
                     }
                   }
+                  eventlink
                 }
               }
             }
           }
         }
       `}
-      render={(data, count) => <TermineBlogRollTemplate data={data} count={count}/>}
+      render={(data, count) => <TermineBlogRollTemplate data={data} count={count} />}
     />
   );
 }
--- src/templates/termine-page.js
+++ src/templates/termine-page.js
@@ -1,81 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { indexOf, kebabCase, lastIndexOf } from "lodash";
+import { kebabCase } from "lodash";
 import { Helmet } from "react-helmet";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
-import { GatsbyImage } from "gatsby-plugin-image";
 import { getImage } from "gatsby-plugin-image";
-
-import "../components/style.css";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 // eslint-disable-next-line
 export const TermineBlogPostTemplate = ({
   content,
   contentComponent,
+  title,
   date,
   eventlink,
-  tags,
-  title,
+  featuredImage,
   helmet,
-  location,
-  featuredimage
 }) => {
   const PostContent = contentComponent || Content;
-  const featImg = getImage(featuredimage) || featuredimage;
+  const image = getImage(featuredImage);
 
   return (
-    <section 
-      className="section flex row center greyBackground"
-      style={{
-        height: "90vh",
-      }}
-    >
+    <section className="section">
       {helmet || ""}
-      <div 
-        className="container content whiteBackground"
-        style={{
-          padding: "5vw",
-          border: "1px solid lightgrey",
-        }}
-      >
+      <div className="container content">
         <div className="columns">
           <div className="column is-10 is-offset-1">
-            <div className="flex row space-between ">
-              <div 
-                className="flex column endEnd"
-                style={{
-                  height: "100%",
-                }}
-              >
-                <h3>{date}</h3>
-                <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-                  {title}
-                </h1>
-                <p>{eventlink ? "Link: " : ""}<a className="link" href={ eventlink ? eventlink : ""} target="_blank">{ eventlink ? eventlink : ""}</a></p>
-                <PostContent content={content} />
-                <br />
-                Ort:&nbsp;
-                <a 
-                  className="link"
-                  href={ location.link }
-                  target="_blank"
-                >
-                  { location.name }
-                </a>
-              </div>
-              <div 
-                style={{
-                  maxWidth: "20vw",
-                }}
-              >
-                <GatsbyImage
-                  image={ featImg }
-                  alt="alt"
-                />
-              </div>              
-            </div>
+            <p style={{ fontWeight: "400", fontSize: "larger" }}>{ title }</p>
+            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+              { title }
+            </h1>
+            <h3>
+              { title }
+            </h3>
+            <GatsbyImage
+              image={ image }
+              alt="alt2"
+              style={{
+                width: "100%",
+                margin: "2vh 0"
+              }}
+            />
+            <div className='finerInnerHTML' dangerouslySetInnerHTML={{ __html: content }}></div>
+            {/* <PostContent content={content} /> */}
+            <br /><br />
+            <div>Lesen Sie den Artikel auf <a href={ eventlink } target="_blank">{ eventlink }</a></div>
           </div>
         </div>
       </div>
@@ -86,12 +56,11 @@ export const TermineBlogPostTemplate = ({
 TermineBlogPostTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   contentComponent: PropTypes.func,
-  date: PropTypes.string,
-  eventlink: PropTypes.string,
   title: PropTypes.string,
+  date: PropTypes.string,
+  featuredImage: PropTypes.object,
+  eventlink: PropTypes.string,
   helmet: PropTypes.object,
-  location: PropTypes.object,
-  featuredimage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 };
 
 const TermineBlogPost = ({ data }) => {
@@ -103,19 +72,17 @@ const TermineBlogPost = ({ data }) => {
         content={post.html}
         contentComponent={HTMLContent}
         helmet={
-          <Helmet titleTemplate={`Termine | ` + post.frontmatter.date}>
-            <title>{`${post.frontmatter.date}`}</title>
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
             <meta
               name="description"
-              content={`${post.frontmatter.description}`}
+              content={``}
             />
           </Helmet>
         }
-        date={post.frontmatter.date}
-        tags={post.frontmatter.tags}
         title={post.frontmatter.title}
-        location={post.frontmatter.location}
-        featuredimage={post.frontmatter.featuredimage}
+        date={post.frontmatter.date}
+        featuredImage={post.frontmatter.featuredimage}
         eventlink={post.frontmatter.eventlink}
       />
     </Layout>
@@ -136,19 +103,16 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
         title
-        eventlink
-        tags
-        location {
-          name
-          link
-        }
+        date
         featuredimage {
           childImageSharp {
-            gatsbyImageData( quality: 100, layout: CONSTRAINED )
+            gatsbyImageData(
+              layout: CONSTRAINED
+            )
           }
         }
+        eventlink
       }
     }
   }
