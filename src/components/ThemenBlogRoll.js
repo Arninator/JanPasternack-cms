8f8ee9b62c79b5fa46ce852b4bb93bfadcd73ed6
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-07-05 18:58:25 +0200

    create everything for themen content

---
+++ src/components/ThemenBlogRoll.js
@@ -0,0 +1,85 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import PreviewCompatibleImage from './PreviewCompatibleImage'
+
+class ThemenBlogRollTemplate extends React.Component {
+  render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
+    // console.log(posts);
+
+    return (
+      <div className="">
+        {posts &&
+          posts.map(({ node: post }) => (
+            <div className="" key={post.id}>
+              <article>
+
+                <h1 className="is-flex is-flex-row" style={{ fontSize: "3rem", marginBottom: "2rem" }}>{ post.frontmatter.title }</h1>
+
+                <div 
+                  className="finerInnerHTML" 
+                  // dangerouslySetInnerHTML={{__html: post.excerpt}}
+                  style={{
+                    fontWeight: "400"
+                  }}
+                >
+                  {post.excerpt}
+                </div>
+                
+              </article>
+              <div className="flex-row flex-center" style={{ margin: "2rem"}}>
+                <Link className="plain-button" to={ post.fields.slug }>
+                  Weiterlesen &rarr;
+                </Link>
+              </div>
+            </div>
+          ))}
+      </div>
+    )
+  }
+}
+
+ThemenBlogRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+
+export default function ThemenBlogRoll() {
+
+  // console.log("aktuellesBlogroll")
+  return (
+    <StaticQuery
+      query={graphql`
+        query ThemenBlogRollQuery {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { frontmatter: { templateKey: { eq: "themen-page" } } }
+          ) {
+            edges {
+              node {
+                excerpt(pruneLength: 500)
+                html
+                id
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                  description
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data, count) => <ThemenBlogRollTemplate data={data} count={count} />}
+    />
+  );
+}
---
+++ src/pages/themen/index.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+
+import Layout from "../../components/Layout";
+import ThemenBlogRoll from "../../components/ThemenBlogRoll";
+
+export default class ThemenIndexPage extends React.Component {
+  componentDidMount() {
+
+    if (document.location.href.includes("blog")) {
+      for (let i = 0; i < document.getElementsByClassName("is-4").length; i++) {
+        document.getElementsByClassName("is-4")[i].classList.add("is-12");
+      }
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/20220504pj209.jpg')`,
+            backgroundPosition: "0 33%"
+          }}
+        >
+          <h1
+            className="red-header"
+            style={{
+              marginRight: "50%",
+            }}
+          >
+            Warum ich kandidiere...
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
+            <div className="content">
+              <ThemenBlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    );
+  }
+}
