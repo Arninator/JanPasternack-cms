eb1aabdd69bfa91ce2e89e295d6f7d6099ce7c48
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-07-06 13:45:17 +0200

    add introduction if neccessary

--- src/pages/themen/index.js
+++ src/pages/themen/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import Layout from "../../components/Layout";
 import ThemenBlogRoll from "../../components/ThemenBlogRoll";
+import Themeneinleitung from "../../components/Themeneinleitung";
 
 export default class ThemenIndexPage extends React.Component {
   componentDidMount() {
@@ -35,6 +36,7 @@ export default class ThemenIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
+              <Themeneinleitung />
               <ThemenBlogRoll />
             </div>
           </div>
