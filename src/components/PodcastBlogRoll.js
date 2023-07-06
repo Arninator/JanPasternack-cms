35bab589cab73df5a566a5b4e34b8a73afd87386
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-07-07 01:30:09 +0200

    adjust podcast components

--- src/components/PodcastBlogRoll.js
+++ src/components/PodcastBlogRoll.js
@@ -5,32 +5,6 @@ import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 class PodcastBlogRollTemplate extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      columns: 4,
-    };
-  }
-
-  componentWillMount() {
-
-    if (typeof window !== "undefined") {
-      if ( window.innerWidth > 992) {
-        this.setState({
-          columns: 4,
-        });
-      } else if ( window.innerWidth > 700 ) {
-        this.setState({
-          columns: 8,
-        });
-      } else {
-        this.setState({
-          columns: 12,
-        });
-      }
-    }
-  }
-
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
@@ -38,65 +12,27 @@ class PodcastBlogRollTemplate extends React.Component {
     // console.log(posts);
 
     return (
-      <div className="columns is-multiline">
-        {posts.length > 0 ?
+      <div className="">
+        {posts &&
           posts.map(({ node: post }) => (
-            <div 
-              className={`is-parent column is-${ this.state.columns }`} 
-              key={post.id}
-            >
-              <article
-                className={`blog-list-item tile is-child notification kachel ${
-                  post.frontmatter.featuredpost ? 'is-featured' : ''
-                }`}
-              >
-                { post.frontmatter.info ? <div style={{ fontWeight: "400" }}>{ post.frontmatter.info }<br /><br /><br /></div> : ""}
-                <header className="flex-column flex-center">
-                  <p 
-                    className="post-meta blog-title"
-                    style={{ 
-                      fontWeight: "400",
-                      fontSize: "1.5rem"
-                    }}
-                  >
-                    { post.frontmatter.title }
-                  </p>
-                  { post.frontmatter.featuredimage ? (
-                    <div style={{ margin: "0 0 2vh 0", minHeight: "25vh", maxHeight: "25vh", overflow: "hidden"}}>
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        }}
-                      />
-                    </div>
-                  ) : null}
-                  <p style={{ margin: "0vh", fontWeight: "400" }}>
-                    { post.frontmatter.subtitle }
-                  </p>
-                </header>
-                <p style={{ fontWeight: "100"}} >
-                  { post.excerpt }
-                  <br />
-                  <br />
-                </p>
-                <div className="flex-row flex-center">
-                  <Link className="button" to={ post.fields.slug }>
-                    Weiterlesen &rarr;
-                  </Link>
+            <div className="" key={post.id}>
+              <article>
+
+                <h1 className="is-flex is-flex-row" style={{ fontSize: "3rem", marginBottom: "2rem", fontWeight: "600"}}>{ post.frontmatter.title }</h1>
+
+                <div 
+                  className="finerInnerHTML" 
+                  // dangerouslySetInnerHTML={{__html: post.excerpt}}
+                  style={{
+                    fontWeight: "400"
+                  }}
+                >
+                  {post.excerpt}
                 </div>
+                
               </article>
             </div>
-          )) : 
-          <div 
-            className="is-parent column is-4"
-            style={{
-              margin: "1vw",
-              color: "darkolivegreen"
-            }}
-          >
-            Aktuell gibt es leider keine Neuigkeiten...
-          </div>}
+          ))} 
       </div>
     )
   }
@@ -124,24 +60,13 @@ export default function PodcastBlogRoll() {
           ) {
             edges {
               node {
-                excerpt(pruneLength: 100)
+                excerpt(pruneLength: 100000)
                 id
                 fields {
                   slug
                 }
                 frontmatter {
-                  info
                   title
-                  subtitle
-                  date
-                  featuredimage {
-                    childImageSharp {
-                      gatsbyImageData(
-                        layout: CONSTRAINED
-                      )
-                    }
-                  }
-                  link
                 }
               }
             }
--- src/pages/podcast/index.js
+++ src/pages/podcast/index.js
@@ -3,6 +3,7 @@ import $ from "jquery";
 
 import Layout from "../../components/Layout";
 import PodcastCover from "../../components/PodcastCover";
+import PodcastBlogRoll from "../../components/PodcastBlogRoll";
 
 export default class PodcastBlogIndexPage extends React.Component {
 
@@ -56,29 +57,22 @@ export default class PodcastBlogIndexPage extends React.Component {
         <PodcastCover />
         <section className="section">
           <div className="container">
-            <h2
-              className="has-text-weight-bold is-size-4 flex"
-              style={{
-                // boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
-                // backgroundColor: "transparent",
-                color: "black",
-                padding: "1rem",
-                marginBottom: "8vh"
-              }}
-            >
-              Podcast
-            </h2>
-            
-            <div id="asd" className="content" style={{ height: ""}}>
-              {/* <PodcastBlogRoll /> */}
+            <PodcastBlogRoll />
+            <br />
+            <br />
+            <br />
+            <div id="asd" className="content">
+              <p style={{ fontWeight: "400", fontSize: "1.5rem"}}>
+                Hier gibt es die aktuelle Folge zum Reinhören:
+              </p>
               <iframe src="https://open.spotify.com/embed/show/4MNRVmUE0Sx552xTez27Fn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-              <iframe src="https://open.spotify.com/embed/episode/0vuVGNjbC6ZFMgxTGM4NdG?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              {/* <iframe src="https://open.spotify.com/embed/episode/0vuVGNjbC6ZFMgxTGM4NdG?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               <iframe src="https://open.spotify.com/embed/episode/7HibTfW5zmjeNAyDmShV52?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               
               
               
               
-              <iframe id="actPodcast" src="https://gudezukunft.podigee.io/45-new-episode/embed?context=website" height="150%" width="100%"></iframe>
+              <iframe id="actPodcast" src="https://gudezukunft.podigee.io/45-new-episode/embed?context=website" height="150%" width="100%"></iframe> */}
               
             </div>
             <iframe style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/playlist/1DkMPXzanSWh5F8PdO8rQQ?utm_source=generator&theme=0" width="100%" height="500vh" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
