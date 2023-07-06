9e4002741d84052f894864c1b0c18ce472b82394
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-07-06 17:08:12 +0200

    try to build nice podcast episodes list

--- src/pages/podcast/index.js
+++ src/pages/podcast/index.js
@@ -1,17 +1,38 @@
 import * as React from "react";
+import $ from "jquery";
 
 import Layout from "../../components/Layout";
 // import PodcastBlogRoll from "../../components/PodcastBlogRoll";
 
 export default class PodcastBlogIndexPage extends React.Component {
+
+  componentDidMount() {
+    let i = 1;
+    let allI = [];
+    let result = $.ajax({
+      type: 'HEAD',
+      url: `https://gudezukunft.podigee.io/${ i }-new-episode/embed?context=website`,
+      success: function() {
+          return true
+      },
+      error: function() {
+          return false
+      }
+    })
+    
+    console.log(result);
+    allI.map(( number ) => {
+      $("#podcast-episodes").append(`<iframe id="actPodcast" src="https://gudezukunft.podigee.io/${ number }-new-episode/embed?context=website" height="" width="100%"></iframe>`)
+    })
+  }
+
   render() {
-;
     return (
       <Layout>
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/podcast-cover.jpg')`,
+            backgroundImage: `url('/img/Twitter Header.jpg')`,
             backgroundPosition: "0% 33%"
           }}
         >
@@ -38,9 +59,39 @@ export default class PodcastBlogIndexPage extends React.Component {
             >
               Podcast
             </h2>
-            <div className="content">
+            
+            <div id="asd" className="content" style={{ height: ""}}>
               {/* <PodcastBlogRoll /> */}
+              <iframe src="https://open.spotify.com/embed/show/4MNRVmUE0Sx552xTez27Fn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              <iframe src="https://open.spotify.com/embed/episode/0vuVGNjbC6ZFMgxTGM4NdG?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              <iframe src="https://open.spotify.com/embed/episode/7HibTfW5zmjeNAyDmShV52?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              
+              
+              
+              
+              <iframe id="actPodcast" src="https://gudezukunft.podigee.io/45-new-episode/embed?context=website" height="150%" width="100%"></iframe>
+              
             </div>
+            <iframe style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/playlist/1DkMPXzanSWh5F8PdO8rQQ?utm_source=generator&theme=0" width="100%" height="500vh" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <div id="podcast-episodes">
+
+            </div>
+            {/* {  
+            $.ajax({
+                type: 'HEAD',
+                url: `https://gudezukunft.podigee.io/${ i }-new-episode/embed?context=website`,
+                success: function() {
+                    return (
+                        <iframe src={`https://gudezukunft.podigee.io/${ i }-new-episode/embed?context=website`} border="0" height="100" width="100%"></iframe>
+                    )
+                },
+                error: function() {
+                    return (
+                      ""
+                    )
+                }
+              })
+            } */}
           </div>
         </section>
       </Layout>
