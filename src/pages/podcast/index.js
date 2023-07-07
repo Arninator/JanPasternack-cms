5f6414a7e493ea886153b61b0bd18663abe8792d
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-07-07 15:30:28 +0200

    insert actual podcast and adjust layout

--- src/pages/podcast/index.js
+++ src/pages/podcast/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import $ from "jquery";
+// import $ from "jquery";
 
 import Layout from "../../components/Layout";
 import PodcastCover from "../../components/PodcastCover";
@@ -68,11 +68,11 @@ export default class PodcastBlogIndexPage extends React.Component {
               <iframe src="https://open.spotify.com/embed/show/4MNRVmUE0Sx552xTez27Fn?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               {/* <iframe src="https://open.spotify.com/embed/episode/0vuVGNjbC6ZFMgxTGM4NdG?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               <iframe src="https://open.spotify.com/embed/episode/7HibTfW5zmjeNAyDmShV52?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              */}
               
               
               
-              
-              <iframe id="actPodcast" src="https://gudezukunft.podigee.io/45-new-episode/embed?context=website" height="150%" width="100%"></iframe> */}
+              {/* <iframe id="actPodcast" src="https://gudezukunft.podigee.io/45-new-episode/embed?context=website" height="400px" width="100%"></iframe> */}
               
             </div>
             <iframe style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/playlist/1DkMPXzanSWh5F8PdO8rQQ?utm_source=generator&theme=0" width="100%" height="500vh" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
